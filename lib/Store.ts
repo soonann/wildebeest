8adaa5391bc19e7e37cad65c32e37b85414da7ab
soonann <sudo@soonann.dev>
2023-03-13 02:26:14 +0800

    feat: initial setup with tailwind

--- lib/Store.ts
+++ lib/Store.ts
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
 )
+
+
---
+++ pages/channels/[id].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router'
+//import { useStore, addMessage } from '~/lib/Store'
+import { useContext, useEffect, useRef } from 'react'
+//import UserContext from '@/lib/UserContext'
+
+export default function Account() {
+    // Render the channels and messages
+    return (<div>Hello world!</div>)
+}
+
