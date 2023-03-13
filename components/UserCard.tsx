2b2180449e0605cabcfcdebefe84f43c1165baa1
soonann <sudo@soonann.dev>
2023-03-14 00:56:47 +0800

    feat: added UserCard with sample landing page

---
+++ components/UserCard.tsx
@@ -0,0 +1,20 @@
+
+const UserCard = ({ results }: any) => {
+
+    return (
+
+        <div className="bg-white rounded-md drop-shadow-lg col-span-1 p-10 justify-center">
+            <div className="text-lg font-semibold">
+                Soon Ann
+            </div>
+            <ul className="text-sm ">
+                <li> Process 1</li>
+                <li> Process 2</li>
+                <li> Process 3</li>
+                <li> Process 4</li>
+            </ul>
+        </div>
+    )
+};
+
+export default UserCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,25 @@
+import UserCard from '@/components/UserCard';
 import { supabase } from '@/lib/Store'
 import { useState } from 'react'
 
 const Home = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleLogin = async (type: string, username: string, password: string) => {
-        try {
-            const { error, data: { user } } =
-                type === 'LOGIN'
-                    ? await supabase.auth.signInWithPassword({ email: username, password })
-                    : await supabase.auth.signUp({ email: username, password })
-            // If the user doesn't exist here and an error hasn't been raised yet,
-            // that must mean that a confirmation email has been sent.
-            // NOTE: Confirming your email address is required by default.
-            if (error) {
-                alert('Error with auth: ' + error.message)
-            } else if (!user) alert('Signup successful, confirmation mail should be sent soon!')
-        } catch (error: any) {
-            console.log('error', error)
-            alert(error.error_description || error)
-        }
-    }
+    const fake = [1, 1, 1, 1, 1];
 
     return (
-        <div className="w-full h-full flex justify-center items-center p-4 bg-gray-300">
-            <div className="w-full sm:w-1/2 xl:w-1/3">
-                <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg bg-white">
-                    <div className="mb-4">
-                        <label className="font-bold text-grey-darker block mb-2">Email</label>
-                        <input
-                            type="text"
-                            className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                            placeholder="Your Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="font-bold text-grey-darker block mb-2">Password</label>
-                        <input
-                            type="password"
-                            className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                            placeholder="Your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
+        <div className='max-w-screen-xl px-4 mx-auto'>
+            <div className='drop-shadow-md h-10 bg-white'>
 
-                    <div className="flex flex-col gap-2">
-                        <a
-                            onClick={(e) => {
-                                e.preventDefault()
-                                handleLogin('SIGNUP', username, password)
-                            }}
-                            href={'/channels'}
-                            className="bg-indigo-700 hover:bg-teal text-white py-2 px-4 rounded text-center transition duration-150 hover:bg-indigo-600 hover:text-white"
-                        >
-                            Sign up
-                        </a>
-                        <a
-                            onClick={(e) => {
-                                e.preventDefault()
-                                handleLogin('LOGIN', username, password)
-                            }}
-                            href={'/channels'}
-                            className="border border-indigo-700 text-indigo-700 py-2 px-4 rounded w-full text-center transition duration-150 hover:bg-indigo-700 hover:text-white"
-                        >
-                            Login
-                        </a>
-                    </div>
-                </div>
+            </div>
+
+            <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 gap-4'>
+
+                <UserCard></UserCard>
+                <UserCard></UserCard>
+                <UserCard></UserCard>
+
             </div>
         </div>
+
     )
 }
 
