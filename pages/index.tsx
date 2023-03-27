4b5afff75db2c80b894f7b101a99fd5853c9758d
soonann <sudo@soonann.dev>
2023-03-27 16:52:59 +0800

    style: scan here for QR Code

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,15 @@ const HomePage = () => {
     return (
         <div className="bg-[url(/background.png)] bg-cover px-4 py-4">
 
-            <div className='grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1'>
+            <div className='grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 place-items-center'>
 
-                <div className='col-span-1 py-5 flex justify-center items-center'>
-                    <img className='rounded-md  w-40 h-40' src='/wildebeest-vercel-qr.jpg' />
+                <div className='col-span-1 py-5 '>
+                    <div className='flex justify-center items-center'>
+                        <span className='text-white text-[1.6rem] text-center font-semibold'>SCAN HERE</span>
+                    </div>
+                    <div className='flex justify-center items-center'>
+                        <img className='rounded-md  w-40 h-40' src='/wildebeest-vercel-qr.jpg' />
+                    </div>
                 </div>
                 <div className='text-7xl font-bold text-white lg:col-span-4 md:col-span-3 sm:col-span-2 col-span-1 md:text-left text-center py-5'>
                     Every designer has their unique<br /> <span className='underline text-white '>design process.</span> What is yours?
